1426999c915ce317c17fb2bdf40307b3719354d1
Remco Gubbels <Remco@guanabana-macbookpro.nl.europe.intranet>
2015-12-22 16:58:40 +0100

    Added bindings & events

--- app/app.component.js
+++ app/app.component.js
@@ -5,11 +5,22 @@
             template: '<h1>My First Angular 2 App</h1>' +
             '<hello-australia-vanilla>' +
                 '<hello-hello>' +
-                    '<span>Transclude this</span>' +
+                    '<span>{{name}}</span>' +
                 '</hello-hello>' +
-            '</hello-australia-vanilla>'
+            '</hello-australia-vanilla>' +
+            '<div *ngIf="showInput">' +
+                '<input [(ngModel)]="name" type="text">' +
+            '</div>'
         })
         .Class({
-            constructor: function() {}
+            constructor: function() {
+                var self = this;
+                self.name = "Remco";
+                self.showInput = false;
+
+                document.addEventListener('name-of-event', function() {
+                    self.showInput = !self.showInput;
+                });
+            }
         });
 })(window.app || (window.app = {}));
